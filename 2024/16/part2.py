840f9a11bae64da54929bde83d9769dedc67ca52
Zac Hayes <zrev2220@gmail.com>
2024-12-19 23:28:43 -0500

    Solve 2024-16 part 2

---
+++ 2024/16/part2.py
@@ -0,0 +1,82 @@
+from collections import defaultdict
+
+grid = []
+WIDTH = None
+
+start: tuple[int, int] = None
+end: tuple[int, int] = None
+try:
+    while True:
+        line = input().strip()
+        WIDTH = len(line)
+        possible_start = line.find("S")
+        if possible_start != -1:
+            start = (len(grid), possible_start)
+        possible_end = line.find("E")
+        if possible_end != -1:
+            end = (len(grid), possible_end)
+
+        grid.append(line)
+except EOFError:
+    pass
+HEIGHT = len(grid)
+
+dirs = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0],
+]
+opposite_dirs = [2, 3, 0, 1]
+start_dir = 0
+
+queue: list[tuple[int, int, int]] = [(*start, start_dir)]
+visited = set()
+distance = defaultdict[tuple[int, int, int], int | None](lambda: None)
+parent = defaultdict[tuple[int, int, int], set[tuple[int, int, int]]](set)
+distance[(*start, start_dir)] = 0
+
+move_cost = 1
+turn_cost = 1000
+
+while queue:
+    current = queue.pop(0)
+    i, j, current_dir = current
+    if current in visited or (i, j) == end:
+        continue
+    current_distance = distance[current]
+    for dir, (di, dj) in enumerate(dirs):
+        if dir == opposite_dirs[current_dir]:
+            continue
+        new_i = i + di
+        new_j = j + dj
+        if 0 <= new_i < HEIGHT and 0 <= new_j < WIDTH and grid[new_i][new_j] != "#":
+            next_pos = (new_i, new_j, dir)
+            existing_distance = distance[next_pos]
+            turns = 0 if dir == current_dir else 1
+            cost = move_cost + turns * turn_cost
+            if existing_distance is None or current_distance + cost < existing_distance:
+                distance[next_pos] = current_distance + cost
+                parent[next_pos] = {current}
+                visited.discard(next_pos)
+            elif existing_distance == current_distance + cost:
+                parent[next_pos].add(current)
+            queue.append(next_pos)
+    visited.add(current)
+
+shortest_end = min(
+    filter(
+        lambda k: distance[k] is not None, [(*end, dir) for dir in range(len(dirs))]
+    ),
+    key=lambda k: distance[k],
+)
+
+path: dict[tuple[int, int], str] = {shortest_end[:2]: "E"}
+p = parent[shortest_end]
+dir_chars = ">v<^"
+while p:
+    pi = p.pop()
+    path[pi[:2]] = dir_chars[pi[2]]
+    p |= parent[pi]
+
+print(len(path))
