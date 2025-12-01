dcbebf24628c29a2f42c610844e9048e76830af3
Zac Hayes <zrev2220@gmail.com>
2025-11-30 19:40:11 -0500

    Test Kotlin on 2015-01

---
+++ 2015/01/part1.kts
@@ -0,0 +1,14 @@
+val input = readln().trim()
+
+var floor = 0
+
+for (char in input) {
+    floor +=
+        when (char) {
+            '(' -> 1
+            ')' -> -1
+            else -> 0
+        }
+}
+
+println(floor)
---
+++ 2015/01/part2.kts
@@ -0,0 +1,22 @@
+import kotlin.system.exitProcess
+
+val input = readln().trim()
+
+var floor = 0
+
+for (i in input.indices) {
+    val char = input[i]
+    floor +=
+        when (char) {
+            '(' -> 1
+            ')' -> -1
+            else -> 0
+        }
+    if (floor < 0) {
+        println(i + 1)
+        exitProcess(0)
+    }
+}
+
+System.err.println("Never entered the basement!")
+exitProcess(1)
