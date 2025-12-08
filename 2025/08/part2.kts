e22dc085420e6a7901b2f4a86ae50e49da8c0a45
Zac Hayes <zrev2220@gmail.com>
2025-12-08 13:23:52 -0500

    Solve 2025-08

---
+++ 2025/08/part2.kts
@@ -0,0 +1,79 @@
+import kotlin.math.pow
+import kotlin.math.sqrt
+import kotlin.system.exitProcess
+
+typealias Node = Triple<Int, Int, Int>
+
+data class DisjointSet(val initialSize: Int) {
+  val parent = MutableList(initialSize) { index -> index }
+  val size = MutableList(initialSize) { 1 }
+  var nSets = initialSize
+
+  fun find(n: Int): Int {
+    if (parent[n] == n) return n
+    parent[n] = find(parent[n])
+    return parent[n]
+  }
+
+  fun union(a: Int, b: Int) {
+    val aParent = find(a)
+    val bParent = find(b)
+    if (aParent == bParent) return
+    parent[bParent] = aParent
+    size[aParent] += size[bParent]
+    nSets--
+  }
+
+  fun getAns(): Long {
+    var remaining = 3
+    var ans = 1L
+    val sortedSizes =
+      size.mapIndexed { index, value -> index to value }.sortedByDescending { it.second }
+    for ((i) in sortedSizes) {
+      if (remaining <= 0) break
+      if (parent[i] != i) continue
+      ans *= size[i]
+      remaining--
+    }
+    return ans
+  }
+}
+
+val nodes = mutableListOf<Node>()
+
+while (true) {
+  val line = readlnOrNull()?.split(",")?.map { it.toInt() } ?: break
+
+  nodes.add(Triple(line[0], line[1], line[2]))
+}
+
+fun dist(a: Node, b: Node): Double =
+  sqrt(
+    (a.first - b.first).toDouble().pow(2) +
+      (a.second - b.second).toDouble().pow(2) +
+      (a.third - b.third).toDouble().pow(2)
+  )
+
+typealias Edge = Triple<Int, Int, Double>
+
+val _edges = mutableListOf<Edge>()
+
+for ((i, node) in nodes.withIndex()) {
+  for (j in i + 1..<nodes.size) {
+    val other = nodes[j]
+    val d = dist(node, other)
+    _edges.add(Triple(i, j, d))
+  }
+}
+
+val edges = _edges.sortedBy { it.third }
+
+val dj = DisjointSet(nodes.size)
+
+for (edge in edges) {
+  dj.union(edge.first, edge.second)
+  if (dj.nSets == 1) {
+    println(nodes[edge.first].first * nodes[edge.second].first)
+    exitProcess(0)
+  }
+}
